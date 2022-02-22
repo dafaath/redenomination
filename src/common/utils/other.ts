eda535f8626b418a92ab565b75878f281743e723
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-02-22 19:29:37 +0700

    menambahkan util sortPhases

--- src/common/utils/other.ts
+++ src/common/utils/other.ts
@@ -1,3 +1,6 @@
+import createHttpError from "http-errors";
+import Phase, { PhaseType } from "../../db/entities/phase.entity";
+
 export function randomString(length: number) {
   let result = "";
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -33,4 +36,14 @@ export function getRandomNumberBetween(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function sortPhases(phases: Phase[]): Phase[] {
+  const phase0 = phases.find((item) => { return item.phaseType === PhaseType.PRE_REDENOM_PRICE })
+  if (!phase0) { throw createHttpError(404, "no preRedenomPrice phase"); }
+  const phase1 = phases.find((item) => { return item.phaseType === PhaseType.TRANSITION_PRICE })
+  if (!phase1) { throw createHttpError(404, "no transitionPrice phase"); }
+  const phase2 = phases.find((item) => { return item.phaseType === PhaseType.POST_REDENOM_PRICE })
+  if (!phase2) { throw createHttpError(404, "no postRedenomPrice phase"); }
+  return [phase0, phase1, phase2];
 }
