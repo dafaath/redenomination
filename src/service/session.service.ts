0e7d3e3a23ffc736f996c94349d2a63538c9f927
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-03-05 14:13:44 +0700

    Revert "username ganda fix?"
    This reverts commit d866e6d72d2277ba50ea81d242ab26664eca09dd.

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -186,7 +186,10 @@ export async function finishSession(
     const session = await Session.findOne(sessionId, {
       relations: ["simulation", "simulation.buyers", "simulation.sellers", "phases"],
     });
-    if (!session) { throw createHttpError(404, "Session with id " + sessionId + " is not found"); }
+
+    if (!session) {
+      throw createHttpError(404, "Session with id " + sessionId + " is not found");
+    }
 
     // Finish Session
     session.isRunning = false;
@@ -198,15 +201,15 @@ export async function finishSession(
       session.avgTrxOccurrence = session.phases.reduce((prev, phase) => prev + Number(phase.avgTrxOccurrence), 0) / Number(session.phases.length);
       session.timeLastRun = new Date(Date.now());
     }
-    const finishedSession = await session.save();
+    const finishedSession = session.save();
 
     // Calculate Simulation Summary
-    const calculatedSimulation = await calcSimulation(session.simulation.id);
+    const calculatedSimulation = calcSimulation(session.simulation.id);
     checkIfError(calculatedSimulation);
 
     // Randomize participant role
-    const buyersUsername = session.simulation.buyers.map(buyer => (buyer.username))
-    const sellersUsername = session.simulation.sellers.map(seller => (seller.username))
+    const buyersUsername = session.simulation.buyers.map(buyer => buyer.username)
+    const sellersUsername = session.simulation.sellers.map(seller => seller.username)
     let participants = [...buyersUsername, ...sellersUsername]
     session.simulation.buyers.forEach(async buyer => {
       let randomNum = Math.floor(Math.random() * participants.length);
